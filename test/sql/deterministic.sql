2fd450e183aa7d98076028b43d4ae5605e93d124
Pierre Senellart <pierre@senellart.com>
2016-07-29 14:53:27 +0800

    Test determinism of provenance

---
+++ test/sql/deterministic.sql
@@ -0,0 +1,11 @@
+\set ECHO none
+set search_path to public, provsql;
+
+CREATE TABLE deterministic_result AS
+  SELECT provenance()=provenance()
+  FROM personal
+  GROUP BY city;
+
+SELECT remove_provenance('deterministic_result');
+SELECT * FROM deterministic_result;
+DROP TABLE deterministic_result;  
