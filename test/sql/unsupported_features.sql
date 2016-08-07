6c47bc3d2ae55aaa7df98bf912783192035d6958
Pierre Senellart <pierre@senellart.com>
2016-08-07 16:46:46 +0800

    Test unsupported features

---
+++ test/sql/unsupported_features.sql
@@ -0,0 +1,15 @@
+\set ECHO none
+
+SELECT * FROM (VALUES (1)) t, personal;
+
+WITH Q(a) AS (SELECT 1) SELECT * FROM Q, personal;
+
+SELECT COUNT(*) FROM personal;
+
+SELECT * FROM personal WHERE city IN (SELECT city FROM personal);
+
+SELECT DISTINCT ON (city) * FROM personal;
+
+SELECT city FROM personal GROUP BY GROUPING SETS ((), (city));
+
+SELECT * FROM personal INTERSECT SELECT * FROM personal;
