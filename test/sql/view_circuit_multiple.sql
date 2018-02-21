3791c6aa05066bb5aa74a63e5c64d6ee7642bbd1
Silviu Maniu <silviu.maniu@lri.fr>
2018-02-21 20:47:06 +0100

    Added missing new tests.

---
+++ test/sql/view_circuit_multiple.sql
@@ -0,0 +1,17 @@
+\set ECHO none
+SET search_path TO public, provsql;
+
+CREATE TABLE vc_result AS
+SELECT city, view_circuit(provenance(),'d') AS gates
+FROM ( 
+  SELECT DISTINCT p1.city FROM
+    personnel p1, personnel p2
+  WHERE
+    p1.id=1 AND
+    p2.id=1
+) t;
+
+SELECT remove_provenance('vc_result');
+
+SELECT city, gates FROM vc_result ORDER BY city;
+DROP TABLE vc_result;
