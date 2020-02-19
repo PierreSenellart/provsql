adadf54079580a57fd7c8e03922e0e38dcad5c77
Pierre Senellart <pierre@senellart.com>
2020-02-19 13:17:33 +0100

    Explicit use of exponential function

--- src/BooleanCircuit.cpp
+++ src/BooleanCircuit.cpp
@@ -399,7 +399,7 @@ double BooleanCircuit::WeightMC(unsigned g, string opt) const {
   //TODO calcul numIterations
 
   //calcul pivotAC
-  double pivotAC=2*ceil(4.4817*(1+1/epsilon)*(1+1/epsilon));
+  final double pivotAC=2*ceil(exp(3./2)*(1+1/epsilon)*(1+1/epsilon));
 
   string cmdline="weightmc --startIteration=0 --gaussuntil=400 --verbosity=0 --pivotAC="+to_string(pivotAC)+ " "+filename+" > "+filename+".out";
 
