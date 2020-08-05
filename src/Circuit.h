0565dbe6edd3fa6e29ae0f467fde24c55982d3c1
Pierre Senellart <pierre@senellart.com>
2020-08-05 10:36:10 +0200

    Refactoring: move definitions of pg_uuid_t into provsql_utils.h, more appropriate than into Circuit.h

--- src/Circuit.h
+++ src/Circuit.h
@@ -1,24 +1,6 @@
 #ifndef CIRCUIT_H
 #define CIRCUIT_H
 
-#include "pg_config.h" // for PG_VERSION_NUM
-#include "c.h" // for int16
-
-extern "C" {
-#include "utils/uuid.h"
-
-#if PG_VERSION_NUM < 100000
-/* In versions of PostgreSQL < 10, pg_uuid_t is declared to be an opaque
- * struct pg_uuid_t in uuid.h, so we have to give the definition of
- * struct pg_uuid_t; this problem is resolved in PostgreSQL 10 */
-#define UUID_LEN 16
-  struct pg_uuid_t
-  {
-    unsigned char data[UUID_LEN];
-  };
-#endif /* PG_VERSION_NUM */
-}  
-
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
