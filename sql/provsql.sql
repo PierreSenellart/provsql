bffffd341c1a4b2de712455d71803729aa08f1bf
Pierre Senellart <pierre@senellart.com>
2020-09-25 17:22:59 +0200

    Fix namespaces

--- sql/provsql.sql
+++ sql/provsql.sql
@@ -77,28 +77,28 @@ DECLARE
 BEGIN
   EXECUTE format('ALTER TABLE %I ADD COLUMN provsql_temp provsql.provenance_token UNIQUE DEFAULT uuid_generate_v4()', _tbl);
   EXECUTE format('CREATE TABLE %I (value DOUBLE PRECISION, provenance provsql.provenance_token)', probability_mapping);
-  LOCK TABLE provenance_circuit_gate;
+  LOCK TABLE provsql.provenance_circuit_gate;
   FOR key IN
     EXECUTE format('SELECT %s AS key FROM %I GROUP BY %s', key_att, _tbl, key_att)
   LOOP
-    prefix_token = gate_one();
+    prefix_token = provsql.gate_one();
     EXECUTE format('SELECT COUNT(*) FROM %I WHERE %s = %L', _tbl, key_att, key.key) INTO nb_rows;
     FOR record IN
       EXECUTE format('SELECT provsql_temp FROM %I WHERE %s = %L', _tbl, key_att, key.key)
     LOOP
       inner_token := uuid_generate_v4();
-      times_token := provenance_times(prefix_token, inner_token);
+      times_token := provsql.provenance_times(prefix_token, inner_token);
       INSERT INTO provsql.provenance_circuit_gate VALUES(inner_token, 'input');
       EXECUTE format('INSERT INTO %I VALUES(%L, %L)', probability_mapping, 1./nb_rows, times_token);
       EXECUTE format('UPDATE %I SET provsql_temp = %L WHERE provsql_temp = %L', _tbl, times_token, record.provsql_temp);
-      prefix_token := provenance_monus(prefix_token, inner_token);
+      prefix_token := provsql.provenance_monus(prefix_token, inner_token);
     END LOOP;  
   END LOOP; 
   EXECUTE format('ALTER TABLE %I RENAME COLUMN provsql_temp TO provsql', _tbl);
   EXECUTE format('CREATE TRIGGER add_provenance_circuit_gate BEFORE INSERT ON %I FOR EACH ROW EXECUTE PROCEDURE provsql.add_provenance_circuit_gate_trigger()',_tbl);
 --  EXECUTE format('ALTER TABLE %I ADD CONSTRAINT provsqlfk FOREIGN KEY (provsql) REFERENCES provsql.provenance_circuit_gate(gate)', _tbl);
 END
-$$ LANGUAGE plpgsql SET search_path=provsql,pg_temp,public SECURITY DEFINER;
+$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION create_provenance_mapping(newtbl text, oldtbl regclass, att text)
   RETURNS void AS
@@ -120,11 +120,11 @@ $$ LANGUAGE SQL IMMUTABLE;
 
 CREATE FUNCTION gate_zero() RETURNS uuid AS
 $$
-  SELECT public.uuid_generate_v5(uuid_ns_provsql(),'zero');
+  SELECT public.uuid_generate_v5(provsql.uuid_ns_provsql(),'zero');
 $$ LANGUAGE SQL IMMUTABLE;
 CREATE FUNCTION gate_one() RETURNS uuid AS
 $$
-  SELECT public.uuid_generate_v5(uuid_ns_provsql(),'one');
+  SELECT public.uuid_generate_v5(provsql.uuid_ns_provsql(),'one');
 $$ LANGUAGE SQL IMMUTABLE;
       
 INSERT INTO provenance_circuit_gate
