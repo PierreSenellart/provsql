f25d39fd22cc9b268e3828ab465ffb2bfd490ec7
Silviu Maniu <silviu@maniu.info>
2020-09-30 16:02:29 +0200

    Agg distinct test.

--- test/sql/agg_distinct.sql
+++ test/sql/agg_distinct.sql
@@ -2,25 +2,14 @@
 SET search_path TO public,provsql;
 
 CREATE TABLE agg_result AS
-  SELECT count(distinct classification)
-    FROM (SELECT name,classification FROM personnel ORDER BY name) b;
+  SELECT city, count(distinct classification)
+    FROM personnel
+    GROUP BY city ORDER BY city;
 
 SELECT remove_provenance('agg_result');
 
-SELECT * FROM agg_result;
+SELECT * FROM agg_result ORDER BY city;
 
-SELECT aggregation_formula(count,'personnel_name') FROM agg_result;
-
-CREATE TABLE agg_result2 AS
-  SELECT city, count(distinct classification)
-    FROM personnel
-    GROUP BY city ORDER BY city, classification;
-
-SELECT remove_provenance('agg_result2');
-
-SELECT * FROM agg_result2 ORDER BY city;
-
-SELECT city, aggregation_formula(count,'personnel_name') FROM agg_result2 ORDER BY city;
+SELECT city, aggregation_formula(count,'personnel_name') FROM agg_result ORDER BY city;
 
 DROP TABLE agg_result;
-DROP TABLE agg_result2;
