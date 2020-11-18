fe096bd51df18d56249ce31b5b31106cd60e5916
Pierre Senellart <pierre@senellart.com>
2020-11-18 12:54:44 +0100

    Make test compatible with PostgreSQL 9

--- test/sql/create_as.sql
+++ test/sql/create_as.sql
@@ -7,7 +7,10 @@ SELECT add_provenance('x');
 
 CREATE TABLE u AS (SELECT * FROM x);
 
-\d u
+SELECT attname, typname
+FROM pg_attribute JOIN pg_type ON atttypid=oid
+WHERE attrelid ='u'::regclass AND attnum>0
+ORDER BY attname;
 
 DROP TABLE x;
 DROP TABLE u;
