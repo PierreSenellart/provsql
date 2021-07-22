cafe030ddf5650560aa894428b541b887d2b75a6
Pierre Senellart <pierre@senellart.com>
2021-07-22 14:18:31 +0200

    Remove useless test for PostgreSQL >= 9.5

--- src/provsql.c
+++ src/provsql.c
@@ -1422,7 +1422,6 @@ static Query *process_query(
     group_by_rewrite = true;
   }
 
-#if PG_VERSION_NUM >= 90500
   /* GROUPING SETS were introduced in version 9.5 of PostgreSQL */
   if (supported && q->groupingSets)
   {
@@ -1439,7 +1438,6 @@ static Query *process_query(
       group_by_rewrite = true;
     }
   }
-#endif /* PG_VERSION_NUM >= 90500 */
 
   if (supported)
   {
