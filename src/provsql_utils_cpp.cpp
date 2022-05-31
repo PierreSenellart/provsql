068ed80348893afc2d92c1b303c4017f53ef1fb4
Pierre Senellart <pierre@senellart.com>
2022-05-31 13:05:45 +0200

    string2uuid

--- src/provsql_utils_cpp.cpp
+++ src/provsql_utils_cpp.cpp
@@ -25,6 +25,57 @@ string uuid2string(pg_uuid_t uuid) {
   return result;
 }
 
+pg_uuid_t string2uuid(const string &source)
+/* copied with small changes from uuid.c */
+{
+  const char *src = source.c_str();
+  pg_uuid_t uuid;
+  bool  braces = false;
+  int i;
+
+  if (src[0] == '{')
+  {
+    src++;
+    braces = true;
+  }
+
+  for (i = 0; i < UUID_LEN; i++)
+  {
+    char str_buf[3];
+
+    if (src[0] == '\0' || src[1] == '\0')
+      goto syntax_error;
+    memcpy(str_buf, src, 2);
+    if (!isxdigit((unsigned char) str_buf[0]) ||
+        !isxdigit((unsigned char) str_buf[1]))
+      goto syntax_error;
+
+    str_buf[2] = '\0';
+    uuid.data[i] = (unsigned char) strtoul(str_buf, NULL, 16);
+    src += 2;
+    if (src[0] == '-' && (i % 2) == 1 && i < UUID_LEN - 1)
+      src++;
+  }
+
+  if (braces)
+  {
+    if (*src != '}')
+      goto syntax_error;
+    src++;
+  }
+
+  if (*src != '\0')
+    goto syntax_error;
+
+  return uuid;
+
+syntax_error:
+  ereport(ERROR,
+      (errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
+       errmsg("invalid input syntax for type %s: \"%s\"",
+         "uuid", src)));
+}
+
 string UUIDDatum2string(Datum token)
 {
   pg_uuid_t *uuid = DatumGetUUIDP(token);
