413979a190cb3b88fe4afeea9958e2e6c81d0f4f
Pierre Senellart <pierre@senellart.com>
2024-02-25 13:03:38 +0100

    Fix demo script

--- doc/demo/demo_script.sql
+++ doc/demo/demo_script.sql
@@ -15,7 +15,7 @@ SELECT DISTINCT P1.city
 FROM Personnel P1 JOIN Personnel P2
 ON P1.city = P2.city
 WHERE P1.id < P2.id;
-SELECT view_circuit(/* Replace this with provenance token of Paris */,'name');
+--SELECT view_circuit(/* Replace this with provenance token of Paris */,'name');
 SELECT create_provenance_mapping('personnel_level','personnel','classification');
 SELECT *,security(provenance(),'personnel_level')
 FROM (
@@ -54,9 +54,9 @@ SELECT *, where_provenance(provenance()) FROM (
 ALTER TABLE personnel ADD COLUMN probability DOUBLE PRECISION;
 UPDATE personnel SET probability=id*1./10;
 SELECT * FROM personnel;
-SELECT create_provenance_mapping('p', 'personnel', 'probability');
+SELECT set_prob(provenance(), probability) from personnel;
 
-SELECT *, probability_evaluate(provenance(),'p','possible-worlds') FROM (
+SELECT *, probability_evaluate(provenance(),'possible-worlds') FROM (
     SELECT DISTINCT city FROM Personnel
   EXCEPT
     SELECT DISTINCT P1.city
@@ -64,7 +64,7 @@ SELECT *, probability_evaluate(provenance(),'p','possible-worlds') FROM (
     ON P1.city = P2.city
     WHERE P1.city = P2.city AND P1.id < P2.id
 ) t;
-SELECT *, probability_evaluate(provenance(),'p','monte-carlo','1000') FROM (
+SELECT *, probability_evaluate(provenance(),'monte-carlo','1000') FROM (
     SELECT DISTINCT city FROM Personnel
   EXCEPT
   SELECT DISTINCT P1.city
@@ -72,9 +72,9 @@ SELECT *, probability_evaluate(provenance(),'p','monte-carlo','1000') FROM (
     ON P1.city = P2.city
     WHERE P1.city = P2.city AND P1.id < P2.id
 ) t;
-SELECT *, probability_evaluate(provenance(),'p','compilation','c2d') FROM (
+SELECT *, probability_evaluate(provenance(),'compilation','c2d') FROM (
     SELECT DISTINCT city FROM Personnel
-  EXCEPT 
+  EXCEPT
     SELECT DISTINCT P1.city
     FROM Personnel P1 JOIN Personnel P2
     ON P1.city = P2.city
@@ -83,22 +83,22 @@ SELECT *, probability_evaluate(provenance(),'p','compilation','c2d') FROM (
 
 SELECT * FROM r;
 SELECT add_provenance('r');
-SELECT create_provenance_mapping('pr', 'r', 'prob');
+SELECT set_prob(provenance(), prob) from r;
 \timing
-SELECT pr1.x,pr2.y,probability_evaluate(provenance(),'pr','possible-worlds')
+SELECT pr1.x,pr2.y,probability_evaluate(provenance(),'possible-worlds')
 FROM r AS pr1, r AS pr2
-WHERE pr2.x=pr1.y AND pr1.x>90 AND pr2.x>90 AND pr2.y>90 
+WHERE pr2.x=pr1.y AND pr1.x>90 AND pr2.x>90 AND pr2.y>90
 GROUP BY pr1.x,pr2.y
 ORDER BY x,y;
 /* CTRL+C */
 /* Roughly 1% additive error with 95% probability */
-SELECT pr1.x,pr2.y,probability_evaluate(provenance(),'pr','monte-carlo','9604')
+SELECT pr1.x,pr2.y,probability_evaluate(provenance(),'monte-carlo','9604')
 FROM r AS pr1, r AS pr2
-WHERE pr2.x=pr1.y AND pr1.x>90 AND pr2.x>90 AND pr2.y>90 
+WHERE pr2.x=pr1.y AND pr1.x>90 AND pr2.x>90 AND pr2.y>90
 GROUP BY pr1.x,pr2.y
 ORDER BY x,y;
-SELECT pr1.x,pr2.y,probability_evaluate(provenance(),'pr','compilation','dsharp')
+SELECT pr1.x,pr2.y,probability_evaluate(provenance(),'compilation','dsharp')
 FROM r AS pr1, r AS pr2
-WHERE pr2.x=pr1.y AND pr1.x>90 AND pr2.x>90 AND pr2.y>90 
+WHERE pr2.x=pr1.y AND pr1.x>90 AND pr2.x>90 AND pr2.y>90
 GROUP BY pr1.x,pr2.y
 ORDER BY x,y;
