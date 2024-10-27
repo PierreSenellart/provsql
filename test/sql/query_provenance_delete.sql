7e08eb7c68d44d2acc9d7f823481c9863e075db9
Albert Ariel Widiaatmaja <albertariel2002@gmail.com>
2024-10-28 02:44:02 +0800

    test: update query_provenance_delete test

--- test/sql/query_provenance_delete.sql
+++ test/sql/query_provenance_delete.sql
@@ -9,6 +9,8 @@ VALUES (1), (2), (3), (4), (5);
 
 SELECT add_provenance('delete_test');
 
+DELETE FROM query_provenance;
+
 -- Test 1: single row deletion
 SELECT create_provenance_mapping('delete_test_id', 'delete_test', 'id');
 SELECT COUNT(*) FROM delete_test_id;
@@ -33,8 +35,8 @@ SELECT create_provenance_mapping('delete_test_id', 'delete_test', 'id');
 SELECT COUNT(*) FROM delete_test_id;
 SELECT value, probability_evaluate(provenance) FROM delete_test_id ORDER BY value;
 
--- Test 3: delete token tracking/ logging into delete_provenance table
-SELECT query FROM delete_provenance ORDER BY deleted_at;
+-- Test 3: delete token tracking/ logging into query_provenance table
+SELECT query FROM query_provenance ORDER BY ts;
 
 DROP TABLE delete_test;
 DROP TABLE delete_test_id;
