d7f5f0f101fec47409280ff3cd55fde265ef4ccc
Pierre Senellart <pierre@senellart.com>
2025-04-18 14:38:38 +0200

    Further optimize compiled semiring

--- src/provenance_evaluate_compiled.cpp
+++ src/provenance_evaluate_compiled.cpp
@@ -24,15 +24,15 @@ PG_FUNCTION_INFO_V1(provenance_evaluate_compiled);
 
 static const char *drop_temp_table = "DROP TABLE IF EXISTS tmp_uuids;";
 
-static Datum pec_varchar(
+template<typename T>
+static void initialize_provenance_mapping(
   const constants_t &constants,
   GenericCircuit &c,
-  gate_t g,
-  const std::set<gate_t> &inputs,
-  const std::string &semiring)
+  std::unordered_map<gate_t, T> &provenance_mapping,
+  const std::function<T(const char *)> &charp_to_value,
+  bool drop_table
+  )
 {
-  std::unordered_map<gate_t, std::string> provenance_mapping;
-
   for (uint64 i = 0; i < SPI_processed; i++) {
     HeapTuple tuple = SPI_tuptable->vals[i];
     TupleDesc tupdesc = SPI_tuptable->tupdesc;
@@ -46,13 +46,28 @@ static Datum pec_varchar(
     char *value = SPI_getvalue(tuple, tupdesc, 1);
     char *uuid = SPI_getvalue(tuple, tupdesc, 2);
 
-    provenance_mapping[c.getGate(uuid)]=std::string(value);
+    provenance_mapping[c.getGate(uuid)]=charp_to_value(value);
 
     pfree(value);
     pfree(uuid);
   }
-  SPI_exec(drop_temp_table, 0);
+  if(drop_table)
+    SPI_exec(drop_temp_table, 0);
   SPI_finish();
+}
+
+static Datum pec_varchar(
+  const constants_t &constants,
+  GenericCircuit &c,
+  gate_t g,
+  const std::set<gate_t> &inputs,
+  const std::string &semiring,
+  bool drop_table)
+{
+  std::unordered_map<gate_t, std::string> provenance_mapping;
+  initialize_provenance_mapping<std::string>(constants, c, provenance_mapping, [](const char *v) {
+    return std::string(v);
+  }, drop_table);
 
   if(semiring=="formula") {
     auto s = c.evaluate<semiring::Formula>(g, provenance_mapping);
@@ -72,30 +87,13 @@ static Datum pec_int(
   GenericCircuit &c,
   gate_t g,
   const std::set<gate_t> &inputs,
-  const std::string &semiring)
+  const std::string &semiring,
+  bool drop_table)
 {
   std::unordered_map<gate_t, unsigned> provenance_mapping;
-
-  for (uint64 i = 0; i < SPI_processed; i++) {
-    HeapTuple tuple = SPI_tuptable->vals[i];
-    TupleDesc tupdesc = SPI_tuptable->tupdesc;
-
-    if (SPI_gettypeid(tupdesc, 2) != constants.OID_TYPE_UUID) {
-      SPI_finish();
-      throw CircuitException("Invalid type for provenance mapping attribute");
-      continue;
-    }
-
-    char *value = SPI_getvalue(tuple, tupdesc, 1);
-    char *uuid = SPI_getvalue(tuple, tupdesc, 2);
-
-    provenance_mapping[c.getGate(uuid)]=atoi(value);
-
-    pfree(value);
-    pfree(uuid);
-  }
-  SPI_exec(drop_temp_table, 0);
-  SPI_finish();
+  initialize_provenance_mapping<unsigned>(constants, c, provenance_mapping, [](const char *v) {
+    return atoi(v);
+  }, drop_table);
 
   if(semiring=="counting") {
     auto val = c.evaluate<semiring::Counting>(g, provenance_mapping);
@@ -104,58 +102,78 @@ static Datum pec_int(
     throw CircuitException("Unknown semiring for type int: "+semiring);
 }
 
-void join_with_temp_uuids(Oid table, const std::vector<std::string> &uuids) {
+bool join_with_temp_uuids(Oid table, const std::vector<std::string> &uuids) {
   if (SPI_connect() != SPI_OK_CONNECT)
     throw CircuitException("SPI_connect failed");
 
-  // 1. Create temp table
-  const char *create_temp_table = "CREATE TEMP TABLE tmp_uuids(id uuid);";
-  if (SPI_exec(create_temp_table, 0) != SPI_OK_UTILITY) {
-    SPI_finish();
-    throw CircuitException("Failed to create temporary table");
-  }
-
-  // 2. Insert UUIDs in batches
-  constexpr size_t batch_size = 500;
-
-  for (size_t offset = 0; offset < uuids.size(); offset += batch_size) {
-    StringInfoData insert_query;
-    initStringInfo(&insert_query);
-    appendStringInfo(&insert_query, "INSERT INTO tmp_uuids VALUES ");
-
-    size_t end = std::min(offset + batch_size, uuids.size());
-    for (size_t i = offset; i < end; ++i) {
-      appendStringInfo(&insert_query, "('%s')%s", uuids[i].c_str(), (i + 1 == end) ? "" : ", ");
-    }
-
-    int retval=SPI_exec(insert_query.data, 0);
-    pfree(insert_query.data);
-
-    if(retval != SPI_OK_INSERT) {
-      SPI_exec(drop_temp_table, 0);
-      SPI_finish();
-      throw CircuitException("Batch insert into temp table failed");
-    }
-  }
-
-  // 3. Join with target table
   char *table_name = get_rel_name(table);
   if (!table_name) {
-    SPI_exec(drop_temp_table, 0);
     SPI_finish();
     throw CircuitException("Invalid OID: no such table");
   }
 
+  constexpr size_t nb_max_uuid_value = 1000;
+
   StringInfoData join_query;
   initStringInfo(&join_query);
-  appendStringInfo(&join_query,
-                   "SELECT value, provenance FROM \"%s\" t JOIN tmp_uuids u ON t.provenance = u.id", table_name);
+  bool drop_table = false;
+
+  // Two different mechanisms to implement the join: if there are less
+  // than nb_max_uuid_value UUIDs, we do a join with a VALUES() list;
+  // otherwise we create a temporary table where we dump the inserts
+  // and join with it.
+  if(uuids.size() <= nb_max_uuid_value) {
+    appendStringInfo(&join_query,
+                     "SELECT value, provenance FROM \"%s\" t JOIN (VALUES", table_name);
+    bool first=true;
+    for(auto u: uuids) {
+      appendStringInfo(&join_query, "%s('%s'::uuid)", first?"":",", u.c_str());
+      first=false;
+    }
+
+    appendStringInfo(&join_query, ") AS u(id) ON t.provenance=u.id");
+  } else {
+    const char *create_temp_table = "CREATE TEMP TABLE tmp_uuids(id uuid);";
+    if (SPI_exec(create_temp_table, 0) != SPI_OK_UTILITY) {
+      SPI_finish();
+      throw CircuitException("Failed to create temporary table");
+    }
+    drop_table = true;
+
+    constexpr size_t batch_size = 1000;
+
+    for (size_t offset = 0; offset < uuids.size(); offset += batch_size) {
+      StringInfoData insert_query;
+      initStringInfo(&insert_query);
+      appendStringInfo(&insert_query, "INSERT INTO tmp_uuids VALUES ");
+
+      size_t end = std::min(offset + batch_size, uuids.size());
+      for (size_t i = offset; i < end; ++i) {
+        appendStringInfo(&insert_query, "('%s')%s", uuids[i].c_str(), (i + 1 == end) ? "" : ",");
+      }
+
+      int retval=SPI_exec(insert_query.data, 0);
+      pfree(insert_query.data);
+
+      if(retval != SPI_OK_INSERT) {
+        SPI_exec(drop_temp_table, 0);
+        SPI_finish();
+        throw CircuitException("Batch insert into temp table failed");
+      }
+    }
+
+    appendStringInfo(&join_query,
+                     "SELECT value, provenance FROM \"%s\" t JOIN tmp_uuids u ON t.provenance = u.id", table_name);
+  }
 
   if (SPI_exec(join_query.data, 0) != SPI_OK_SELECT) {
-    SPI_exec(drop_temp_table, 0);
+    if(drop_table)
+      SPI_exec(drop_temp_table, 0);
     SPI_finish();
     throw CircuitException("Join query failed");
   }
+
+  return drop_table;
 }
 
 static Datum provenance_evaluate_compiled_internal
@@ -168,14 +186,14 @@ static Datum provenance_evaluate_compiled_internal
   std::transform(inputs.begin(), inputs.end(), std::back_inserter(inputs_uuid), [&c](auto x) {
     return c.getUUID(x);
   });
-  join_with_temp_uuids(table, inputs_uuid);
+  bool drop_table = join_with_temp_uuids(table, inputs_uuid);
 
   constants_t constants = get_constants(true);
 
   if(type==constants.OID_TYPE_VARCHAR)
-    return pec_varchar(constants, c, g, inputs, semiring);
+    return pec_varchar(constants, c, g, inputs, semiring, drop_table);
   else if(type==constants.OID_TYPE_INT)
-    return pec_int(constants, c, g, inputs, semiring);
+    return pec_int(constants, c, g, inputs, semiring, drop_table);
   else
     throw CircuitException("Unknown element type for provenance_evaluate_compiled");
 }
