813ec10296a431aa9a0428b2ca479384681deeaf
Pierre Senellart <pierre@senellart.com>
2025-04-24 13:56:10 +0200

    Typos in error messages

--- src/CircuitFromMMap.cpp
+++ src/CircuitFromMMap.cpp
@@ -21,14 +21,14 @@ static C getCircuitFromMMap(pg_uuid_t token, char message_char)
 
   unsigned long size;
   if(!READB(size, unsigned long))
-    elog(ERROR, "Cannot write to pipe (message type %c)", message_char);
+    elog(ERROR, "Cannot read from pipe (message type %c)", message_char);
 
   char *buf = new char[size], *p = buf;
   ssize_t actual_read, remaining_size=size;
   while((actual_read=read(provsql_shared_state->pipembr, p, remaining_size))<remaining_size) {
     if(actual_read<=0) {
       provsql_shmem_unlock();
-      elog(ERROR, "Cannot write to pipe (message type %c)", message_char);
+      elog(ERROR, "Cannot read from pipe (message type %c)", message_char);
     } else {
       remaining_size-=actual_read;
       p+=actual_read;
